2a6fea5118d1a35d40a98322ebfb28eeb3169089
MathisBicheyre <mathis.bicheyre@efrei.net>
2021-10-10 17:00:53 +0200

    moved words out of the code

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
 // Run dotenv
 require('dotenv').config();
 
+// fs and readline for file reading
+const fs = require('fs');
+const readline = require('readline');
+
 // Discord client
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
@@ -13,12 +17,35 @@ client.on('ready', () => {
 // Token
 client.login(process.env.DISCORD_TOKEN);
 
+// Read line interface
+const rl = readline.createInterface({
+    input: fs.createReadStream('data/words.txt'),
+    output: process.stdout,
+    terminal: false
+});
+
+// List of words and responses
+var pairings = [];
+
+// Read each line and store them in memory
+rl.on('line', (line) => {
+    const split = line.split('-');
+    var pairing = [];
+    split.forEach(word => {
+        pairing.push(word);
+    });
+    pairings.push(pairing);
+});
+
 // Process message
-client.on('message', message  => {
+client.on('messageCreate', message  => {
 	var string = message.content.replace(/[^\w\s]/gi, '');
 	string.toLowerCase();
-	if(string.endsWith("quoi"))
-		message.channel.send("feur");
-	else if(string.endsWith("oui"))
-		message.channel.send("stiti");
+	pairings.every(pairing => {
+    if(string.endsWith(pairing[0])){
+      message.channel.send(pairing[1]);
+      return false;
+    }
+    return true;
+  });
 });
